e43818a9d7702e7effc01b1c07344f0bac54c1d9
Teppei Taguchi <tetzng.tt@gmail.com>
2025-12-21 06:37:20 +0900

    fix(activate): update fish script

--- src/cmd/activate.rs
+++ src/cmd/activate.rs
@@ -6,11 +6,14 @@ pub(crate) fn run_fish() {
     print!("{}", fish_script());
 }
 
-fn fish_script() -> &'static str {
+fn fish_script() -> String {
+    let version = env!("CARGO_PKG_VERSION");
     // Guard against multiple sourcing and wrap pez to emit events in-process.
-    r#"
-if not set -q __pez_fish_activated
-    set -g __pez_fish_activated 1
+    format!(
+        r#"
+set -l __pez_version "{version}"
+if not set -q __pez_activate_version; or test "$__pez_activate_version" != "$__pez_version"
+    set -g __pez_activate_version $__pez_version
 
     function __pez_fish_split_subcmd --description "Find subcommand and args"
         set -l args $argv
@@ -32,7 +35,7 @@ if not set -q __pez_fish_activated
             case '--jobs'
                 set i (math $i + 2)
                 continue
-            case '--jobs='*
+            case '--jobs=*'
                 set i (math $i + 1)
                 continue
             end
@@ -70,36 +73,37 @@ if not set -q __pez_fish_activated
         switch $subcmd
         case install
             env PEZ_SUPPRESS_EMIT=1 command pez $argv
-            set -l status $status
-            if test $status -eq 0
+            set -l exit_status $status
+            if test $exit_status -eq 0
                 __pez_fish_source_and_emit install install $subargs
             end
-            return $status
+            return $exit_status
         case update upgrade
             env PEZ_SUPPRESS_EMIT=1 command pez $argv
-            set -l status $status
-            if test $status -eq 0
+            set -l exit_status $status
+            if test $exit_status -eq 0
                 __pez_fish_source_and_emit update $subcmd $subargs
             end
-            return $status
+            return $exit_status
         case uninstall remove
             if contains -- --stdin $subargs
                 set -l stdin_file (cat | psub -f -s .pez_uninstall)
                 cat $stdin_file | __pez_fish_source_and_emit uninstall $subcmd $subargs
                 cat $stdin_file | env PEZ_SUPPRESS_EMIT=1 command pez $argv
-                set -l status $status
-                return $status
+                set -l exit_status $status
+                return $exit_status
             end
             __pez_fish_source_and_emit uninstall $subcmd $subargs
             env PEZ_SUPPRESS_EMIT=1 command pez $argv
-            set -l status $status
-            return $status
+            set -l exit_status $status
+            return $exit_status
         case '*'
             command pez $argv
         end
     end
 end
 "#
+    )
 }
 
 #[cfg(test)]
@@ -109,7 +113,9 @@ mod tests {
     #[test]
     fn script_contains_guard_and_suppress_flag() {
         let text = fish_script();
-        assert!(text.contains("__pez_fish_activated"));
+        assert!(text.contains("__pez_activate_version"));
+        assert!(text.contains("__pez_version"));
+        assert!(text.contains(env!("CARGO_PKG_VERSION")));
         assert!(text.contains("PEZ_SUPPRESS_EMIT"));
         assert!(text.contains("command pez files --dir conf.d --from"));
         assert!(text.contains("__pez_fish_split_subcmd"));
@@ -147,6 +153,7 @@ mod tests {
     fn global_flags_are_skipped() {
         let text = fish_script();
         assert!(text.contains("--jobs"));
+        assert!(text.contains("--jobs=*"));
         assert!(text.contains("--verbose"));
         assert!(text.contains("^-[v]+$"));
     }
